27e6636fe86d55c68b8c4c6a170966c1b8cac8f9
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2022-09-05 22:11:44 +0200

    Clean up, comments

--- starterOnly/js/form.js
+++ starterOnly/js/form.js
@@ -1,31 +1,16 @@
-// DOM Elements
-const firstName = document.getElementById('first');
-const lastName = document.getElementById('last');
-const email = document.getElementById('email');
-const birthdate = document.getElementById('birthdate');
-const quantity = document.getElementById('quantity');
-const locations = [...document.querySelectorAll('input[type="radio"]')];
-console.log(locations[0]);
-const checkbox = document.getElementById('checkbox1');
-
-// Messages
-const firstNameErrorMessage = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
-const lastNameErrorMessage = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
-const emailErrorMessage = 'Veuillez entrer une adresse mail valide.';
-const quantityErrorMessage = 'Veuillez indiquer le nombre de tournois.';
-const locationsErrorMessage = 'Vous devez choisir une option.';
-const birthdateErrorMessage = 'Vous devez entrer votre date de naissance.';
-const termsOfServiceErrorMessage = 'Vous devez vérifier que vous acceptez les termes et conditions.';
-
-// 
+//  
 let valid = true;
 
+/* form object contains for each input:
+    - DOM element
+    - error message
+    - a validation function
+*/
 const form = [
     {
         element: document.getElementById('first'),
         errorMessage: 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.',
         validation: function() {
-            // console.log('first name: ' + this.element.value);
             return /^[a-z][a-z]+$/i.test(this.element.value.trim());
         }
     },
@@ -33,7 +18,6 @@ const form = [
         element: document.getElementById('last'),
         errorMessage: 'Veuillez entrer 2 caractères ou plus pour le champ du nom.',
         validation: function() {
-            // console.log('last name: ' + this.element.value);
             return /^[a-z][a-z]+$/i.test(this.element.value.trim());
         }
     },
@@ -81,49 +65,8 @@ const form = [
     }
 ];
 
-// String validation RegEx for first and last names
-const validateName = (element) => {
-    return !/^[a-z][a-z]+$/i.test(element.value.trim());
-}
-
-// Email validation RegEx
-const validateEmail = (element) => {
-    return !/^[\w]+@[\w]+\.[\w]+$/.test(element.value); /*email.value.indexOf('@') == -1*/
-}
-
-// Check if quantity is a number
-const validateTournamentQuantity = (element) => {
-    return isNaN(element.value) || element.value === '';
-}
-
-// 
-const validateLocationChoice = (element) => {
-    let checked = false;
-    for(let location of element) {
-        console.log(element);
-        if(location.checked) {
-            console.log(location.checked);
-            checked = true;
-            break;
-        }
-    }
-    return checked;
-}
-
-// Terms of Service checkbox validation
-const validateTermsOfService = (element) => {
-    return !element.checked;
-};
-
-// Date object validation
-const validateDate = (element) => {
-    return isNaN(Date.parse(element.value));
-};
-
 // 
 const validateInput = (element, message, validation) => {
-
-    // console.log(`${element} ${message} ${validation}`);
 
     if(!validation) {
         showError(element, message);
@@ -135,27 +78,12 @@ const validateInput = (element, message, validation) => {
 const validate = (event) => {
     valid = true;
 
-    // form.forEach(item = () => validateInput(item.validation, item.))
     for(let item in form) {
-        // valid = form[item].validation();
-        // console.log(`${form[item].element} ${form[item].errorMessage} ${form[item].validation()}`);
         if(Array.isArray(form[item].element)) {
             console.log('is array');
             validateInput(form[item].element[0], form[item].errorMessage, form[item].validation());
         } else validateInput(form[item].element, form[item].errorMessage, form[item].validation());
-
-        // for(let key in form[item]) {
-        //     console.log(`${form[item][key]}`);
-        // }
     }
-
-    // validateInput(validateName(firstName), firstName, firstNameErrorMessage);
-    // validateInput(validateName(lastName), lastName, lastNameErrorMessage);
-    // validateInput(validateEmail(email), email, emailErrorMessage);
-    // validateInput(validateDate(birthdate), birthdate, birthdateErrorMessage);
-    // validateInput(validateLocationChoice(), locations[0], locationsErrorMessage);
-    // validateInput(validateTournamentQuantity(quantity), quantity, quantityErrorMessage);
-    // validateInput(validateTermsOfService(), checkbox, termsOfServiceErrorMessage);
 
     console.log(`form is ${valid ? 'valid' : 'invalid'}`);
 
